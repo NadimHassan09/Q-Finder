fab06029d90ec7a1540f88c3929c8d016380691a
NadimHassan09 <es-nadim.hassan2027@alexu.edu.eg>
2025-08-13 13:06:03 +0300

    added color mode toggle button
    I've programmed the color mode toggle button using Java script to switch between bright and dark mode themes

---
+++ Js/script.js
@@ -0,0 +1,54 @@
+const root = document.documentElement;
+const btn = document.getElementById("modeToggle");
+
+let darkMode = true;
+
+
+function colorModeToggle(){
+    if (darkMode){
+        root.style.setProperty("--color-text-white","#111834");
+        root.style.setProperty("--color-white","#111834");
+        root.style.setProperty("--color-black","white");
+        root.style.setProperty("--color-dark-1","#FFFFFF");
+        root.style.setProperty("--color-dark-2","#F2F3F5");
+        root.style.setProperty("--color-dark-3","#EEF1F6");
+        root.style.setProperty("--color-dark-4","#F7F8FB");
+        root.style.setProperty("--color-dark-5","#F5F7FA");
+        root.style.setProperty("--color-dark-6","#FBEEEE");
+        root.style.setProperty("--banner-color-dark-1","#EEF1F6");
+        root.style.setProperty("--banner-color-dark-2","#F7F8FB");
+        root.style.setProperty("--tag-color-1","#EEF1F6");
+        root.style.setProperty("--download-btn-color-1","#1D2735");
+        root.style.setProperty("--color-gray-1","#111834");
+        root.style.setProperty("--color-gray-2","#4E5562");
+        root.style.setProperty("--color-gray-3","#797E89");
+        root.style.setProperty("--color-gray-4","#797E89");
+        root.style.setProperty("--color-gray-5","#E5E5E5");
+        root.style.setProperty("--color-gray-6","#E5E5E5");
+        darkMode = false
+    }
+    else {
+        root.style.setProperty("--color-text-white","white");
+        root.style.setProperty("--color-white","white");
+        root.style.setProperty("--color-black","black");
+        root.style.setProperty("--color-dark-1","#111827");
+        root.style.setProperty("--color-dark-2","#242A38");
+        root.style.setProperty("--color-dark-3","#333D4C");
+        root.style.setProperty("--color-dark-4","#343D4A");
+        root.style.setProperty("--color-dark-5","#1D2735");
+        root.style.setProperty("--color-dark-6","#251E2B");
+        root.style.setProperty("--banner-color-dark-1","#1D2735");
+        root.style.setProperty("--banner-color-dark-2","#4E5562");
+        root.style.setProperty("--tag-color-1","#333D4C");
+        root.style.setProperty("--download-btn-color-1","#333D4C");
+        root.style.setProperty("--color-gray-1","#cdccce");
+        root.style.setProperty("--color-gray-2","#cdced0");
+        root.style.setProperty("--color-gray-3","#959CA7");
+        root.style.setProperty("--color-gray-4","#9CA3AF");
+        root.style.setProperty("--color-gray-5","#4E5562");
+        root.style.setProperty("--color-gray-6","#343A47");
+        darkMode = true
+    }
+}
+
+btn.addEventListener("click", colorModeToggle);
